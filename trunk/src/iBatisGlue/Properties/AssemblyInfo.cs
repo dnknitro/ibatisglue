c26ddbaea22aaa8dfc36e69fb7eda2842aa8c9ac
dnk.nitro@gmail.com <dnk.nitro@gmail.com@be31223e-f043-11de-88d6-afc75ad17f39>
2011-12-14 16:32:47 +0000

    Version info

--- trunk/src/iBatisGlue/Properties/AssemblyInfo.cs
+++ trunk/src/iBatisGlue/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "iBatisGlue.ConsoleApp" )]
+[assembly: AssemblyTitle( "iBatisGlue" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "dnk" )]
-[assembly: AssemblyProduct( "iBatisGlue.ConsoleApp" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "iBatisGlue" )]
 [assembly: AssemblyCopyright( "Copyright ©  2009 Volodymyr Shcherbyna" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.2" )]
-[assembly: AssemblyFileVersion( "1.0.0.2" )]
+[assembly: AssemblyVersion( "1.0.0.3" )]
+[assembly: AssemblyFileVersion( "1.0.0.3" )]
